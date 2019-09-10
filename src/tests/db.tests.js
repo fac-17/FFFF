4ce37a34ea6ded252d7f0345834340eea6248bbd
gminova <minova1995@gmail.com>
2019-09-10 16:19:40 +0100

    set up testing env for database

---
+++ src/tests/db.tests.js
@@ -0,0 +1,7 @@
+const test = require('tape');
+
+test('Check that we\'re ready for database testing', t => {
+    t.assert(true, true, 'Must return true');
+    t.assert(process.env.NODE_ENV, 'test', 'Must be in testing environment');
+    t.end();
+});
