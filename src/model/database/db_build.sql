268c33929f4ea02773cf6a0544e0164aca3d40ad
Jan Skowronski <crianonim@gmail.com>
2019-09-10 15:20:28 +0100

    Fixes create table order relates #23

--- src/model/database/db_build.sql
+++ src/model/database/db_build.sql
@@ -7,28 +7,30 @@ DROP TABLE IF EXISTS flags CASCADE;
 DROP TABLE IF EXISTS entries CASCADE;
 
 
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     category_id INTEGER REFERENCES categories (id)
 );
-CREATE TABLE categories (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
 CREATE TABLE supermarkets (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL
+);
+CREATE TABLE flags (
+    id SERIAL PRIMARY KEY,
+    emoji_code VARCHAR(100) NOT NULL
 );
 CREATE TABLE origins (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     flag_id INTEGER REFERENCES flags (id)
 );
-CREATE TABLE flags (
-    id SERIAL PRIMARY KEY,
-    emoji_code VARCHAR(100) NOT NULL
-);
+
 CREATE TABLE entries (
     id SERIAL PRIMARY KEY,
     product_id INTEGER REFERENCES products (id),
