7da5b62e97a091e06f531608830009cc023efdfd
Jack Bridger <jacksbridger@gmail.com>
2019-09-10 15:46:19 +0100

    fix linting issues by removing some unused variables Relates #23

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const exphbs = require('express-handlebars');
 require('dotenv').config();
 //import helpers
--- src/tests/router.tests.js
+++ src/tests/router.tests.js
@@ -1,6 +1,6 @@
 const test = require('tape');
-const supertest = require('supertest');
-const app = require('../app');
+// const supertest = require('supertest');
+// const app = require('../app');
 
 test('Check that we\'re ready for router testing', t => {
     t.assert(true, true, 'Must return true');
