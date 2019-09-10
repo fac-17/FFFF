e54b71d803b4a645a32a3b01e7aa5da9d41bb3cb
andy <andy.william.mcdonald@gmail.com>
2019-09-10 16:48:21 +0100

    db test created relates #58

--- src/tests/db.tests.js
+++ src/tests/db.tests.js
@@ -1,7 +1,20 @@
-const test = require('tape');
+const test = require("tape");
+const runBuild = require("../model/database/db_build");
 
-test('Check that we\'re ready for database testing', t => {
-    t.assert(true, true, 'Must return true');
-    t.assert(process.env.NODE_ENV, 'test', 'Must be in testing environment');
-    t.end();
+test("Check that we're ready for database testing", t => {
+  t.assert(true, true, "Must return true");
+  t.assert(process.env.NODE_ENV, "test", "Must be in testing environment");
+  t.end();
+});
+
+test("Check database build is successful", t => {
+  runBuild((err) => {
+    if (err) {
+      t.error(err, "Database build failed");
+      t.end();
+    } else {
+      t.pass("Database build passing");
+      t.end();
+    }
+  });
 });
