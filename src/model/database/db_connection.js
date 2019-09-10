f784ba1dc8da91c0f7c5251806b0a25bdb640afb
Jack Bridger <jacksbridger@gmail.com>
2019-09-10 12:27:20 +0100

    Setup of db_connection

---
+++ src/model/database/db_connection.js
@@ -0,0 +1,23 @@
+const { Pool } = require("pg");
+const url = require("url");
+
+let DATABASE_URL = process.env.DATABASE_URL;
+if (process.env.NODE_ENV === "test") {
+  DATABASE_URL = process.env.TEST_DB_URL;
+}
+if (!DATABASE_URL) throw new Error("Environmental variable must be set");
+
+const params = url.parse(DATABASE_URL);
+const [username, password] = params.auth.split(":");
+
+const options = {
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split("/")[1],
+  max: process.env.DATABASE_MAX_CONNECTIONS || 2,
+  user: username,
+  password,
+  ssl: params.hostname !== "localhost"
+};
+
+module.exports = new Pool(options);
