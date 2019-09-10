9cbe53607b2c399c04222a9e5bce4877026dd20e
Jack Bridger <jacksbridger@gmail.com>
2019-09-10 12:40:01 +0100

    setup of db_init.js Relates #22

---
+++ src/model/database/db_init.js
@@ -0,0 +1,8 @@
+const runBuild = require("./db_build");
+runBuild((err, res) => {
+  if (err) {
+    console.log("database creation error", err);
+  } else {
+    console.log("database created", res);
+  }
+});
