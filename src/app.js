06cca3a50eeb05e61f989e8724dd7c72fb64e518
andy <andy.william.mcdonald@gmail.com>
2019-09-10 12:20:24 +0100

    added .env relates #49

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const exphbs = require('express-handlebars');
-
+require('dotenv').config();
+   
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
