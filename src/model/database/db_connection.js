b944938cbd2579812aecba116086b5256e0a8d69
Jack Bridger <jacksbridger@gmail.com>
2019-09-10 12:54:41 +0100

    Implementing Gigi amend #22

--- src/model/database/db_connection.js
+++ src/model/database/db_connection.js
@@ -2,13 +2,14 @@ const { Pool } = require("pg");
 const url = require("url");
 require("dotenv").config();
 
-let DATABASE_URL = process.env.DATABASE_URL;
-if (process.env.NODE_ENV === "test") {
-  DATABASE_URL = process.env.TEST_DB_URL;
-}
-if (!DATABASE_URL) throw new Error("Environmental variable must be set");
+let connectionString =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
 
-const params = url.parse(DATABASE_URL);
+if (!connectionString) throw new Error("Database url must be set");
+
+const params = url.parse(connectionString);
 const [username, password] = params.auth.split(":");
 
 const options = {
