6a68f4a95854a5e589acab2b1ac86aed9aa785e9
Jack Bridger <jacksbridger@gmail.com>
2019-09-10 17:09:54 +0100

    pass test for select Relates #33

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const home = require('./home');
+const select = require('./select');
 
-router.get('/',home.get)
+router.get('/', home.get);
+router.get('/select', select.get);
 
 module.exports = router;
