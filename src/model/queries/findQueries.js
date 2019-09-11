5d5a16eff9e102418d757faafbf5073221c4d667
gminova <minova1995@gmail.com>
2019-09-11 01:35:52 +0100

    added promise version of selectAllFoodItems along with passing test

--- src/model/queries/findQueries.js
+++ src/model/queries/findQueries.js
@@ -13,4 +13,13 @@ const findAllFoodItems = cb => {
     )
 }
 
-module.exports = { findAllFoodItems }
+const findAllFoodItemsPromise = new Promise ((resolve, reject) => {
+    dbConnection.query(
+        'SELECT products.name pname, categories.name cname FROM products INNER JOIN categories ON products.category_id = categories.id;',
+        (err, res) => {
+            if (err) reject(err);
+            resolve(res.rows);
+        });
+});
+
+module.exports = { findAllFoodItems, findAllFoodItemsPromise }
