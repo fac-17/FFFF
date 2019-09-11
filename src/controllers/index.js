c5fafb352483008db29df4cb20873210b20b4833
Jan Skowronski <crianonim@gmail.com>
2019-09-11 10:21:01 +0100

    Implement route /results, closes #34

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const home = require('./home');
 const select = require('./select');
+const results = require('./results')
 
 router.get('/', home.get);
 router.get('/select', select.get);
+router.get('/results', results.get);
 
 module.exports = router;
