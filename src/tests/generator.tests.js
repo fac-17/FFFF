3c972160b4688f1837666cc7258d2dbe732ab59e
gminova <minova1995@gmail.com>
2019-09-11 15:31:06 +0100

    typo

--- src/tests/generator.tests.js
+++ src/tests/generator.tests.js
@@ -16,7 +16,7 @@ test('Check generator returns an array', t => {
 
 test('Check generator returns an array with appropriate length', t => {
     const actual = generator(6, 5, 7).length;
-    const expected = 36;
+    const expected = 30;
     t.deepEquals(expected, actual, 'Check gen returns an array');
     t.end();
 });
