84a2fb80a27a8c86ca412253709aa90c0937eea7
gminova <minova1995@gmail.com>
2019-09-12 17:30:55 +0100

    shopping list visualized

--- public/js/shoppingSelection.js
+++ public/js/shoppingSelection.js
@@ -8,15 +8,25 @@ allItems.forEach(elem => {
             selected.push(e.target.dataset.value);
             const newInput = document.createElement('input');
             newInput.type = 'hidden';
-            newInput.classList.add("input"+e.target.dataset.value);
+            newInput.classList.add("input" + e.target.dataset.value);
             newInput.name = 'product'
             newInput.value = e.target.dataset.value;
             shoppingForm.appendChild(newInput)
+
+            const shoppingList = document.querySelector(".shopping-list");
+            const listItem = document.createElement("li");
+            listItem.dataset.name = e.target.dataset.name;
+            listItem.innerText = e.target.dataset.name;
+            shoppingList.appendChild(listItem);
         } else {
-            const elemToDelete = document.querySelector('.input'+ e.target.dataset.value);
+            const elemToDelete = document.querySelector('.input' + e.target.dataset.value);
             elemToDelete.parentNode.removeChild(elemToDelete);
             const indexToDelete = selected.indexOf(e.target.dataset.value);
-            selected.splice(indexToDelete,1);
-            }
-        });   
-    })
+            selected.splice(indexToDelete, 1);
+
+
+            const listItemDelete = document.querySelector(".shopping-list [data-name='"+e.target.dataset.name+"']")
+            listItemDelete.parentNode.removeChild(listItemDelete);
+        }
+    });
+})
