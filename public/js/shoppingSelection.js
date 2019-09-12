7bdfd85375a9faa7507ebda19fcc57c31527282d
andy <andy.william.mcdonald@gmail.com>
2019-09-12 16:34:09 +0100

    selection and deletion working relates #40

--- public/js/shoppingSelection.js
+++ public/js/shoppingSelection.js
@@ -1,18 +1,22 @@
-const appleButton = document.querySelector('.add-apple');
 const shoppingForm = document.querySelector('.shopping-form');
 const allItems = document.querySelector('.item-list').childNodes;
-console.log(allItems);
-
-// appleButton.addEventListener('click', () => {
-//     const newInput = document.createElement('input');
-//     newInput.type = 'text';
-//     newInput.name = 'product';
-//     newInput.value = 'Apple';
-//     shoppingForm.appendChild(newInput)
-// })
+const selected = [];
 
 allItems.forEach(elem => {
     elem.addEventListener('click', (e) => {
-        console.log(e.target);
+        if (!selected.includes(e.target.dataset.value)) {
+            selected.push(e.target.dataset.value);
+            const newInput = document.createElement('input');
+            newInput.type = 'hidden';
+            newInput.classList.add("input"+e.target.dataset.value);
+            newInput.name = 'product'
+            newInput.value = e.target.dataset.value;
+            shoppingForm.appendChild(newInput)
+        } else {
+            const elemToDelete = document.querySelector('.input'+ e.target.dataset.value);
+            elemToDelete.parentNode.removeChild(elemToDelete);
+            const indexToDelete = selected.indexOf(e.target.dataset.value);
+            selected.splice(indexToDelete,1);
+            }
+        });   
     })
-})
