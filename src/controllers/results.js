98b122d8943804d909fe9545bf268452ebf5fec9
gminova <minova1995@gmail.com>
2019-09-12 15:27:53 +0100

    results page is being populated with database data relates #42

--- src/controllers/results.js
+++ src/controllers/results.js
@@ -1,13 +1,24 @@
+const { resultsArray } = require('../model/queries/resultsQuery');
 exports.post = (req, res) => {
     console.log(req.body);
-   
-   
-   
-    const dummyData = [
-        {supermarket: "Asda", rating: "mostly sourced locally"},
-        {supermarket: "Tesco", rating: "mostly not sourced locally"},
-        {supermarket: "Lidl", rating: "mostly sourced from abroad"}
-    ];
-    res.render("results", { dummyData });
+    const body = {
+        products: ['1', '7', '8']
+    }
+
+    const productIds = body.products.map(Number);
+    console.log(productIds);
+    resultsArray(productIds)
+        .then(result => {
+            console.log(result)
+            res.render("results", { result });
+        });
+
+
+    // const dummyData = [
+    //     { supermarket: "Asda", rating: "mostly sourced locally" },
+    //     { supermarket: "Tesco", rating: "mostly not sourced locally" },
+    //     { supermarket: "Lidl", rating: "mostly sourced from abroad" }
+    // ];
+    // res.render("results", { dummyData });
     // res.send('hello results')
 };
