bfc8ef6195d521203854f4e625edcac0542c89a9
Jan Skowronski <crianonim@gmail.com>
2019-09-12 16:33:47 +0100

    Use real post values in results controller relates #42

--- src/controllers/results.js
+++ src/controllers/results.js
@@ -1,11 +1,11 @@
 const { resultsArray } = require('../model/queries/resultsQuery');
 exports.post = (req, res) => {
-    console.log(req.body);
-    const body = {
-        products: ['1', '7', '8']
-    }
+    // console.log(req.body);
+    // const body = {
+    //     product: ['1', '7', '8']
+    // }
 
-    const productIds = body.products.map(Number);
+    const productIds = req.body.product.map(Number);
     console.log(productIds);
     resultsArray(productIds)
         .then(result => {
