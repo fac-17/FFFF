0cbb59d0e7b142e6644e8d3b63f7125606ecfe0c
Jack Bridger <jacksbridger@gmail.com>
2019-09-16 11:58:41 +0100

    error message when no items selected Relates #98

--- src/controllers/results.js
+++ src/controllers/results.js
@@ -1,12 +1,17 @@
 const { resultsArray } = require('../model/queries/resultsQuery');
 exports.post = (req, res) => {
-
-    const correctArray = Array.isArray(req.body.product) ? req.body.product : [req.body.product]; 
-    const productIds = correctArray.map(Number);
-    console.log(productIds);                                                        
-    resultsArray(productIds)
-        .then(result => {
-            console.log(result)
-            res.render("results", { result, back:"/select" });
-        });
+    console.log(req.body.product);
+    if (req.body.product === undefined) {
+        res.render('error', { errorMessage: 'No items selected by user' });
+    }
+    else {
+        const correctArray = Array.isArray(req.body.product) ? req.body.product : [req.body.product];
+        const productIds = correctArray.map(Number);
+        console.log(productIds);
+        resultsArray(productIds)
+            .then(result => {
+                console.log(result)
+                res.render("results", { result, back: "/select" });
+            });
+    }
 };
