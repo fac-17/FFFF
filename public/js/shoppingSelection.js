10ec916b3bacb7ce22f2704e65b55c23166b51e0
andy <andy.william.mcdonald@gmail.com>
2019-09-16 12:59:19 +0100

    hide submit button if nothing selected relates #98

--- public/js/shoppingSelection.js
+++ public/js/shoppingSelection.js
@@ -1,10 +1,14 @@
 const shoppingForm = document.querySelector('.shopping-form');
 const allItems = document.querySelector('.item-list').childNodes;
 const selected = [];
+const submitButton = document.querySelector('.submit-button');
 
 allItems.forEach(elem => {
     elem.addEventListener('click', (e) => {
         if (!selected.includes(e.currentTarget.dataset.value)) {
+            if (selected.length === 0) {
+                submitButton.classList.toggle("hidden");
+            }
             selected.push(e.currentTarget.dataset.value);
             const newInput = document.createElement('input');
             newInput.type = 'hidden';
@@ -18,13 +22,15 @@ allItems.forEach(elem => {
             listItem.dataset.name = e.currentTarget.dataset.name;
             listItem.innerText = e.currentTarget.dataset.name;
             shoppingList.appendChild(listItem);
-
+            
         } else {
+            if (selected.length === 1) {
+                submitButton.classList.toggle("hidden");
+            }
             const elemToDelete = document.querySelector('.input' + e.currentTarget.dataset.value);
             elemToDelete.parentNode.removeChild(elemToDelete);
             const indexToDelete = selected.indexOf(e.currentTarget.dataset.value);
             selected.splice(indexToDelete, 1);
-            
             
             const listItemDelete = document.querySelector(".shopping-list [data-name='"+e.currentTarget.dataset.name+"']")
             listItemDelete.parentNode.removeChild(listItemDelete);
