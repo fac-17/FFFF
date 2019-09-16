9e3d418440d9f1ca8d28930e0c85b82df9692a82
gminova <minova1995@gmail.com>
2019-09-16 16:10:24 +0100

    fixed back button

--- src/controllers/itemsbysupermarket.js
+++ src/controllers/itemsbysupermarket.js
@@ -1,11 +1,13 @@
-const query=require('../model/queries/breakdownQuery').breakdownQuery
+const query = require('../model/queries/breakdownQuery').breakdownQuery
 exports.get = (req, res) => {
     let productIds = req.query.product;
-    productIds = typeof productIds === 'string' ? productIds = [productIds]: productIds;
+    productIds = typeof productIds === 'string' ? productIds = [productIds] : productIds;
     productIds = productIds.map(Number);
-    query(productIds,Number(req.query.supermarket)).then(result=>{
-        res.render('itemsbysupermarket', {result:result.rows,
-        supermarket:result.rows[0].sname});
-        console.log(result.rows)
+    query(productIds, Number(req.query.supermarket)).then(result => {
+        res.render('itemsbysupermarket', {
+            result: result.rows,
+            back: `${req.url.split('itemsbysupermarket').join('results')}`,
+            supermarket: result.rows[0].sname
+        });
     });
 }
