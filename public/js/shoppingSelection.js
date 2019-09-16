f9524690e56e7e656c9b8158551d55750c19c43e
Jack Bridger <jacksbridger@gmail.com>
2019-09-16 14:40:32 +0100

    Remove duplication, use add and remove class instead of toggle Relates #98 #103

--- public/js/shoppingSelection.js
+++ public/js/shoppingSelection.js
@@ -7,7 +7,7 @@ allItems.forEach(elem => {
     elem.addEventListener('click', (e) => {
         if (!selected.includes(e.currentTarget.dataset.value)) {
             if (selected.length === 0) {
-                submitButton.classList.toggle("hidden");
+                submitButton.classList.remove("hidden");
             }
             selected.push(e.currentTarget.dataset.value);
             const newInput = document.createElement('input');
@@ -22,17 +22,17 @@ allItems.forEach(elem => {
             listItem.dataset.name = e.currentTarget.dataset.name;
             listItem.innerText = e.currentTarget.dataset.name;
             shoppingList.appendChild(listItem);
-            
+
         } else {
             if (selected.length === 1) {
-                submitButton.classList.toggle("hidden");
+                submitButton.classList.add("hidden");
             }
             const elemToDelete = document.querySelector('.input' + e.currentTarget.dataset.value);
             elemToDelete.parentNode.removeChild(elemToDelete);
             const indexToDelete = selected.indexOf(e.currentTarget.dataset.value);
             selected.splice(indexToDelete, 1);
-            
-            const listItemDelete = document.querySelector(".shopping-list [data-name='"+e.currentTarget.dataset.name+"']")
+
+            const listItemDelete = document.querySelector(".shopping-list [data-name='" + e.currentTarget.dataset.name + "']")
             listItemDelete.parentNode.removeChild(listItemDelete);
         }
         e.currentTarget.classList.toggle("selected-item")
--- src/controllers/results.js
+++ src/controllers/results.js
@@ -15,14 +15,4 @@ exports.get = (req, res) => {
                 res.render("results", { result, queryString, back: "/select" });
             });
     }
-
-    console.log(req.query)
-    const correctArray = Array.isArray(req.query.product) ? req.query.product : [req.query.product];
-    const productIds = correctArray.map(Number);
-    console.log(productIds);
-    resultsArray(productIds)
-        .then(result => {
-            console.log(result)
-            res.render("results", { result, back: "/select" });
-        });
 };
