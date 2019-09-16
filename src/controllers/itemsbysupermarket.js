ad2d8a40937c1f394de7b4ea8654211030b5f576
Jack Bridger <jacksbridger@gmail.com>
2019-09-16 16:38:43 +0100

    minor change to ternary operator Relates #106

--- src/controllers/itemsbysupermarket.js
+++ src/controllers/itemsbysupermarket.js
@@ -1,7 +1,7 @@
 const query = require('../model/queries/breakdownQuery').breakdownQuery
 exports.get = (req, res) => {
     let productIds = req.query.product;
-    productIds = typeof productIds === 'string' ? productIds = [productIds] : productIds;
+    productIds = typeof productIds === 'string' ? [productIds] : productIds;
     productIds = productIds.map(Number);
     query(productIds, Number(req.query.supermarket)).then(result => {
         res.render('itemsbysupermarket', {
