7d7ac51de8efdc4da7413954ea1c0bfb0d820bf4
gminova <minova1995@gmail.com>
2019-09-16 16:54:03 +0100

    error pages relates #36

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,10 +4,13 @@ const home = require('./home');
 const select = require('./select');
 const results = require('./results');
 const itemsbysupermarket = require('./itemsbysupermarket');
+const error = require('./error');
 
 router.get('/', home.get);
 router.get('/select', select.get);
 router.get('/results', results.get);
 router.get('/itemsbysupermarket', itemsbysupermarket.get);
+router.use(error.client);
+router.use(error.server);
 
 module.exports = router;
