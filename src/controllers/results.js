afcda2605e8fb5fbf3f4f49357003792cc88b365
Jan Skowronski <crianonim@gmail.com>
2019-09-16 12:09:48 +0100

    Creates link between supermarkets in results and breakdown. closes #99

--- src/controllers/results.js
+++ src/controllers/results.js
@@ -3,10 +3,12 @@ exports.post = (req, res) => {
 
     const correctArray = Array.isArray(req.body.product) ? req.body.product : [req.body.product]; 
     const productIds = correctArray.map(Number);
-    console.log(productIds);                                                        
+    const queryString= productIds.map(id=>"product="+id).join("&")
+    console.log(productIds,{queryString});
+
     resultsArray(productIds)
         .then(result => {
             console.log(result)
-            res.render("results", { result, back:"/select" });
+            res.render("results", { result, queryString ,back:"/select" });
         });
 };
