7b94a2260031e89c57282c21a761de599b74af9e
Jack Bridger <jacksbridger@gmail.com>
2019-09-17 18:00:33 +0100

    reformatting the shopping selection js file to be more modularised Relates #104

--- public/js/shoppingSelection.js
+++ public/js/shoppingSelection.js
@@ -16,13 +16,15 @@ const addRemoveClasses = (addRemove, elemArray, className) => {
         }
     })
 }
+// remove the class hidden from a specified element
 const removeHidden = (elemArray) => {
     addRemoveClasses('remove', elemArray, 'hidden');
 }
+// add the class hidden from a specified element
 const addHidden = (elemArray) => {
     addRemoveClasses('add', elemArray, 'hidden');
 }
-
+// create an input with value itemValue and append a given value to the hidden form
 const appendToForm = (form, itemValue) => {
     const newInput = document.createElement('input');
     newInput.type = 'hidden';
@@ -32,6 +34,7 @@ const appendToForm = (form, itemValue) => {
     form.appendChild(newInput)
 }
 
+// create a list item with name/text itemName and append a given value to the shopping list
 const appendToList = (list, itemName) => {
     const listItem = document.createElement("li");
     listItem.dataset.name = itemName;
@@ -39,32 +42,51 @@ const appendToList = (list, itemName) => {
     list.appendChild(listItem);
 }
 
+// Remove an item from the shopping list 
+const deleteListItem = (itemName) => {
+    const listItemDelete = document.querySelector(".shopping-list [data-name='" + itemName + "']")
+    listItemDelete.parentNode.removeChild(listItemDelete);
+}
+// Remove a given input elem from the hidden form 
+const deleteElem = (itemValue) => {
+    const elemToDelete = document.querySelector('.input' + itemValue);
+    elemToDelete.parentNode.removeChild(elemToDelete);
+    const indexToDelete = selected.indexOf(itemValue);
+    selected.splice(indexToDelete, 1);
+}
+// when an unselected shopping item is clicked, if it's first item, 
+// reveal the submit button & shopping list
+// update the list of selected values and call updateForm, updateList
+const addShoppingItem = (itemName, itemValue) => {
+    if (selected.length === 0) {
+        removeHidden([submitButton, shoppingListWrapper])
+    }
+    selected.push(itemValue);
+    appendToForm(shoppingForm, itemValue);
+    appendToList(shoppingList, itemName);
+}
+// when an item already selected is clicked, if it's the only item selected, 
+// hide submit button and shopping list Then delete the hidden input form and the
+// list item from the shopping list
+const removeShoppingItem = (itemName, itemValue) => {
+    if (selected.length === 1) {
+        addHidden([submitButton, shoppingListWrapper]);
+    }
+    deleteElem(itemValue);
+    deleteListItem(itemName);
+}
+// main function to check if an item has been selected already and to respond appropriately
+// Then toggle the selected-item class. 
 const toggleItem = (itemName, itemValue, classList) => {
     if (!selected.includes(itemValue)) {
-        if (selected.length === 0) {
-            removeHidden([submitButton, shoppingListWrapper])
-        }
-        selected.push(itemValue);
-        appendToForm(shoppingForm, itemValue);
-        appendToList(shoppingList, itemName);
-
-
+        addShoppingItem(itemName, itemValue);
     } else {
-        if (selected.length === 1) {
-            submitButton.classList.add("hidden");
-            shoppingListWrapper.classList.add("hidden");
-            addHidden([submitButton, shoppingListWrapper]);
-        }
-        const elemToDelete = document.querySelector('.input' + itemValue);
-        elemToDelete.parentNode.removeChild(elemToDelete);
-        const indexToDelete = selected.indexOf(itemValue);
-        selected.splice(indexToDelete, 1);
-
-        const listItemDelete = document.querySelector(".shopping-list [data-name='" + itemName + "']")
-        listItemDelete.parentNode.removeChild(listItemDelete);
+        removeShoppingItem(itemName, itemValue);
     }
-    classList.toggle("selected-item")
+    classList.toggle("selected-item");
 }
+
+// add toggle item event listener to each item that can be bought
 allItems.forEach(elem => {
     elem.addEventListener('click', (e) => {
         toggleItem(e.currentTarget.dataset.name, e.currentTarget.dataset.value, e.currentTarget.classList)
