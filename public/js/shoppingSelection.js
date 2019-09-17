27ece05624a47939462f7fb30519f43f84bc671a
Jack Bridger <jacksbridger@gmail.com>
2019-09-17 17:20:51 +0100

    removing appendToList and appendToForm from main func Relates #104

--- public/js/shoppingSelection.js
+++ public/js/shoppingSelection.js
@@ -5,31 +5,55 @@ const submitButton = document.querySelector('.submit-button');
 const shoppingList = document.querySelector(".shopping-list");
 const shoppingListWrapper = document.querySelector(".shopping-list--wrapper")
 
+// add/remove a named class to/from an array of elements
+const addRemoveClasses = (addRemove, elemArray, className) => {
+    elemArray.forEach(elem => {
+        if (addRemove === 'add') {
+            elem.classList.add(className);
+        }
+        else if (addRemove === 'remove') {
+            elem.classList.remove(className);
+        }
+    })
+}
+const removeHidden = (elemArray) => {
+    addRemoveClasses('remove', elemArray, 'hidden');
+}
+const addHidden = (elemArray) => {
+    addRemoveClasses('add', elemArray, 'hidden');
+}
 
+const appendToForm = (form, itemValue) => {
+    const newInput = document.createElement('input');
+    newInput.type = 'hidden';
+    newInput.classList.add("input" + itemValue);
+    newInput.name = 'product'
+    newInput.value = itemValue;
+    form.appendChild(newInput)
+}
+
+const appendToList = (list, itemName) => {
+    const listItem = document.createElement("li");
+    listItem.dataset.name = itemName;
+    listItem.innerText = itemName;
+    list.appendChild(listItem);
+}
 
 const toggleItem = (itemName, itemValue, classList) => {
     if (!selected.includes(itemValue)) {
         if (selected.length === 0) {
-            submitButton.classList.remove("hidden");
-            shoppingListWrapper.classList.remove("hidden")
+            removeHidden([submitButton, shoppingListWrapper])
         }
         selected.push(itemValue);
-        const newInput = document.createElement('input');
-        newInput.type = 'hidden';
-        newInput.classList.add("input" + itemValue);
-        newInput.name = 'product'
-        newInput.value = itemValue;
-        shoppingForm.appendChild(newInput)
+        appendToForm(shoppingForm, itemValue);
+        appendToList(shoppingList, itemName);
 
-        const listItem = document.createElement("li");
-        listItem.dataset.name = itemName;
-        listItem.innerText = itemName;
-        shoppingList.appendChild(listItem);
 
     } else {
         if (selected.length === 1) {
             submitButton.classList.add("hidden");
             shoppingListWrapper.classList.add("hidden");
+            addHidden([submitButton, shoppingListWrapper]);
         }
         const elemToDelete = document.querySelector('.input' + itemValue);
         elemToDelete.parentNode.removeChild(elemToDelete);
@@ -46,3 +70,4 @@ allItems.forEach(elem => {
         toggleItem(e.currentTarget.dataset.name, e.currentTarget.dataset.value, e.currentTarget.classList)
     })
 })
+
