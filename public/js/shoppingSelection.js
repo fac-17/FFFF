1db543896db9a5a031c3f4101c8e208426acb172
Jack Bridger <jacksbridger@gmail.com>
2019-09-17 16:58:17 +0100

    some progress on refactor - passing in arguments to the click event Relates #104

--- public/js/shoppingSelection.js
+++ public/js/shoppingSelection.js
@@ -5,39 +5,44 @@ const submitButton = document.querySelector('.submit-button');
 const shoppingList = document.querySelector(".shopping-list");
 const shoppingListWrapper = document.querySelector(".shopping-list--wrapper")
 
+
+
+const toggleItem = (itemName, itemValue, classList) => {
+    if (!selected.includes(itemValue)) {
+        if (selected.length === 0) {
+            submitButton.classList.remove("hidden");
+            shoppingListWrapper.classList.remove("hidden")
+        }
+        selected.push(itemValue);
+        const newInput = document.createElement('input');
+        newInput.type = 'hidden';
+        newInput.classList.add("input" + itemValue);
+        newInput.name = 'product'
+        newInput.value = itemValue;
+        shoppingForm.appendChild(newInput)
+
+        const listItem = document.createElement("li");
+        listItem.dataset.name = itemName;
+        listItem.innerText = itemName;
+        shoppingList.appendChild(listItem);
+
+    } else {
+        if (selected.length === 1) {
+            submitButton.classList.add("hidden");
+            shoppingListWrapper.classList.add("hidden");
+        }
+        const elemToDelete = document.querySelector('.input' + itemValue);
+        elemToDelete.parentNode.removeChild(elemToDelete);
+        const indexToDelete = selected.indexOf(itemValue);
+        selected.splice(indexToDelete, 1);
+
+        const listItemDelete = document.querySelector(".shopping-list [data-name='" + itemName + "']")
+        listItemDelete.parentNode.removeChild(listItemDelete);
+    }
+    classList.toggle("selected-item")
+}
 allItems.forEach(elem => {
     elem.addEventListener('click', (e) => {
-        if (!selected.includes(e.currentTarget.dataset.value)) {
-            if (selected.length === 0) {
-                submitButton.classList.remove("hidden");
-                shoppingListWrapper.classList.remove("hidden")
-            }
-            selected.push(e.currentTarget.dataset.value);
-            const newInput = document.createElement('input');
-            newInput.type = 'hidden';
-            newInput.classList.add("input" + e.currentTarget.dataset.value);
-            newInput.name = 'product'
-            newInput.value = e.currentTarget.dataset.value;
-            shoppingForm.appendChild(newInput)
-
-            const listItem = document.createElement("li");
-            listItem.dataset.name = e.currentTarget.dataset.name;
-            listItem.innerText = e.currentTarget.dataset.name;
-            shoppingList.appendChild(listItem);
-
-        } else {
-            if (selected.length === 1) {
-                submitButton.classList.add("hidden");
-                shoppingListWrapper.classList.add("hidden");
-            }
-            const elemToDelete = document.querySelector('.input' + e.currentTarget.dataset.value);
-            elemToDelete.parentNode.removeChild(elemToDelete);
-            const indexToDelete = selected.indexOf(e.currentTarget.dataset.value);
-            selected.splice(indexToDelete, 1);
-
-            const listItemDelete = document.querySelector(".shopping-list [data-name='" + e.currentTarget.dataset.name + "']")
-            listItemDelete.parentNode.removeChild(listItemDelete);
-        }
-        e.currentTarget.classList.toggle("selected-item")
-    });
+        toggleItem(e.currentTarget.dataset.name, e.currentTarget.dataset.value, e.currentTarget.classList)
+    })
 })
