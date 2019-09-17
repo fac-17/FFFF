358a0a95d738c4fa50f37991285ffb0fff46a114
gminova <minova1995@gmail.com>
2019-09-17 17:13:17 +0100

    helpers to display lcally sources labels, image and small link to show breakdown

---
+++ src/views/helpers/scorer.js
@@ -0,0 +1,8 @@
+module.exports = scorer = score => {
+    switch(Math.round(score)) {
+        case 1: return "plane"
+        case 2: return "lorry"
+        case 3: return "car"
+        case 4: return "bike"
+    }
+}
