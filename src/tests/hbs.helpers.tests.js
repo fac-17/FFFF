6e0b70fd21b118f7f5d051597a30ec59c35525f1
Jan Skowronski <crianonim@gmail.com>
2019-09-18 10:50:09 +0100

    Add tests for imagenamer. relates #128

--- src/tests/hbs.helpers.tests.js
+++ src/tests/hbs.helpers.tests.js
@@ -9,3 +9,11 @@ test("Check if rounder rounds a string",t=>{
     t.equal(helpers.rounder('1.12345'),'1.12',"rounds correct")
     t.end();
 })
+test("Check if imagenamer provides good filename for Apple",t=>{
+    t.equal(helpers.imagenamer('Apple'),'apple','Apple becomes apple');
+    t.end();
+})
+test("Check if imagenamer provides good filename for White Cabbage",t=>{
+    t.equal(helpers.imagenamer('White Cabbage'),'white_cabbage','White Cabbage becomes white_cabbage');
+    t.end();
+})
